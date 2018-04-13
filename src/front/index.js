e93dd165fc5bd4578d04f924601685e7d2339afb
Robert Talamantez <robert.talamantez2@gmail.com>
2018-04-13 12:30:55 -0700

    refactor 3js into obj

--- src/front/index.js
+++ src/front/index.js
@@ -16,8 +16,139 @@ import { timeSeries } from "pondjs";
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 
+let muppeteer;
 let index = 0;
 let attention = 0; // init attention value
+
+
+let Muppeteer = function(){
+  var self = this;
+
+  // THREE.js stuff
+  self.container;
+  self.camera;
+  // self.controls;
+  self.scene;
+  self.renderer;
+  self.geometry;
+  self.difference;
+  self.light;
+
+  self.objects = [];
+  self.init = function() {
+
+      self.container = document.getElementById('container');
+      document.body.appendChild(self.container);
+
+      self.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+      self.camera.position.z = 1000;
+
+      self.scene = new THREE.Scene();
+      self.scene.background = new THREE.Color(0xf0f0f0);
+      self.scene.add(new THREE.AmbientLight(0x505050));
+
+      self.light = new THREE.SpotLight(0xffffff, 1.5);
+      self.light.position.set(0, 500, 2000);
+      self.light.castShadow = true;
+      self.light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 200, 10000));
+      self.light.shadow.bias = -0.00022;
+      self.light.shadow.mapSize.width = 2048;
+      self.light.shadow.mapSize.height = 2048;
+      self.scene.add(self.light);
+
+      self.geometry = new THREE.BoxGeometry(40, 40, 40);
+      self.renderer = new THREE.WebGLRenderer({ antialias: true });
+      self.renderer.setPixelRatio(window.devicePixelRatio);
+      self.renderer.setSize(window.innerWidth, window.innerHeight);
+      self.renderer.shadowMap.enabled = true;
+      self.renderer.shadowMap.type = THREE.PCFShadowMap;
+
+      self.container.appendChild(self.renderer.domElement);
+
+      window.addEventListener('resize', self.onWindowResize, false);
+
+      self.render = function() {
+          // controls.update();
+          console.log('rendering 3js scene')
+          self.renderer.render(self.scene, self.camera);
+
+      }
+      self.onWindowResize = function() {
+
+          self.camera.aspect = window.innerWidth / window.innerHeight;
+          self.camera.updateProjectionMatrix();
+
+          self.renderer.setSize(window.innerWidth, window.innerHeight);
+
+      }
+
+      self.animate();
+  }
+
+  self.drawCubes = function(geometry, attention, objects) {
+          console.log('drawing cubes');
+          if(self.scene){
+            if(self.scene.children){
+              while(self.scene.children.length > 0){
+                self.scene.remove(self.scene.children[0]);
+              }
+            }
+          }
+
+          if(self.container){
+            // console.dir(self.container);
+          }else{
+            console.log('self.container not found')
+          }
+          console.dir(self.scene);
+          self.scene.add(new THREE.AmbientLight(0x505050));
+
+          self.light = new THREE.SpotLight(0xffffff, 1.5);
+          self.light.position.set(0, 500, 2000);
+          self.light.castShadow = true;
+          self.light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 200, 10000));
+          self.light.shadow.bias = -0.00022;
+          self.light.shadow.mapSize.width = 2048;
+          self.light.shadow.mapSize.height = 2048;
+          self.scene.add(self.light);
+          for (let i = 0; i < attention; i++) {
+              let object = new THREE.Mesh(self.geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
+
+              object.position.x = Math.random() * 1000 - 500;
+              object.position.y = Math.random() * 600 - 300;
+              object.position.z = Math.random() * 800 - 400;
+
+              object.rotation.x = Math.random() * 2 * Math.PI;
+              object.rotation.y = Math.random() * 2 * Math.PI;
+              object.rotation.z = Math.random() * 2 * Math.PI;
+
+              object.scale.x = Math.random() * 2 + 1;
+              object.scale.y = Math.random() * 2 + 1;
+              object.scale.z = Math.random() * 2 + 1;
+
+              object.castShadow = true;
+              object.receiveShadow = true;
+              object.name = 'cube' + i;
+
+              self.scene.add(object);
+              self.objects.push(object);
+      }
+  }
+
+
+  self.animate = function() {
+      // console.log('animating');
+      requestAnimationFrame(self.animate);
+
+      self.render();
+
+  }
+
+
+  self.init();
+}
+
+
 const SET_BRAIN_DATA = 'SET_BRAIN_DATA'; // set action value
 // init app state
 const initialState = {
@@ -26,30 +157,33 @@ const initialState = {
   lastAttention: 0,
   change: 0,
   eeg: {},
-  meditation: 0,
-
+  meditation: 0
 }
 
 const store = createStore(attentionApp); // initialize store
 
 // app
 function attentionApp(state = initialState, action) {
-  console.log(action);
-    switch(action.type){
-      case 'SET_BRAIN_DATA':
-        let attention = action.object.attention.attention;
-        let eeg = action.object.eeg.eeg;
-        if(action.object){
-          console.log(action.object)
-        }
-        return Object.assign({}, state, {
-          attention: attention,
-          lastAttention: state.attention,
-          change: -(state.attention-attention),
-          eeg: eeg
-        })
-      default:
-        return state;
+    if(!action){
+      throw new Error('action is not working, dear programmer')
+    }
+    else{
+      switch(action.type){
+        case 'SET_BRAIN_DATA':
+          let attention = action.object.attention.attention;
+          let eeg = action.object.eeg.eeg;
+          // if(action.object){
+          //   console.log(action.object)
+          // }
+          return Object.assign({}, state, {
+            attention: attention,
+            lastAttention: state.attention,
+            change: -(state.attention-attention),
+            eeg: eeg
+          })
+        default:
+          return state;
+      }
     }
 }
 // action creator: update attention
@@ -63,7 +197,7 @@ function updateBrainData(object){
 const socket = io('http://127.0.0.1:4000'); // initialize websocket
 
 socket.on('data', function(data){
-    console.dir(data);
+    // console.dir(data);
     attention = data._source._buffers[2][0] || null;
       store.dispatch(
         updateBrainData(
@@ -77,8 +211,6 @@ socket.on('data', function(data){
   return;
 });
 
-
-
 let fireAttentionRequest = function(){
   // console.log('firing brain data request')
   new Promise(function(resolve, reject) {
@@ -86,17 +218,21 @@ let fireAttentionRequest = function(){
   }).then(
     function(state){
       // wipe and redraw 3js Cubes
-      drawCubes(geometry, state.attention, state.lastAttention, objects);
+      if(!muppeteer){
+        console.log('waiting for muppeteer to drawCubes');
+      }else{
+        muppeteer.drawCubes(muppeteer.geometry, state.attention, state.lastAttention, muppeteer.objects);
+      }
+      // console.log('Fired Attention Request. state:');
+      // console.dir(state);
       // render React Components and Trigger request
       // for next data point
-      renderAndRequest(state);
+      render(state);
     }
-  )
+  ).then( getData )
 }
 
-
-
-function renderAndRequest(state){
+function render(state){
   const element = (
     <Grid item xs={6} sm={3}>
       <div id="data">
@@ -112,120 +248,19 @@ function renderAndRequest(state){
       </div>
     </Grid>
   );
-
   ReactDOM.render(
     element,
     document.querySelector('#root')
   );
-  socket.emit('getData')
+}
+
+function getData() {
+  socket.emit('getData');
 }
 // once per second, log the state,render a new element, and emit a call for the next reading
 setInterval(fireAttentionRequest, 1000);
-
-
-// THREE.js stuff
-let container;
-let camera, controls, scene, renderer, geometry, difference;
-let objects = [];
-
-document.body.onload = init;
-
-function init() {
-
-    container = document.getElementById('container');
-    document.body.appendChild(container);
-
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
-
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xf0f0f0);
-    scene.add(new THREE.AmbientLight(0x505050));
-
-    let light = new THREE.SpotLight(0xffffff, 1.5);
-    light.position.set(0, 500, 2000);
-    light.castShadow = true;
-    light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 200, 10000));
-    light.shadow.bias = -0.00022;
-    light.shadow.mapSize.width = 2048;
-    light.shadow.mapSize.height = 2048;
-    scene.add(light);
-
-    geometry = new THREE.BoxGeometry(40, 40, 40);
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFShadowMap;
-
-    container.appendChild(renderer.domElement);
-
-    window.addEventListener('resize', onWindowResize, false);
-    animate();
-}
-
-function drawCubes(geometry, attention, testAttention, objects) {
-        // console.log('drawCubes');
-
-        // wipe objects
-        while(scene.children.length > 0){
-            scene.remove(scene.children[0]);
-        }
-
-        scene.add(new THREE.AmbientLight(0x505050));
-
-        let light = new THREE.SpotLight(0xffffff, 1.5);
-        light.position.set(0, 500, 2000);
-        light.castShadow = true;
-        light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 200, 10000));
-        light.shadow.bias = -0.00022;
-        light.shadow.mapSize.width = 2048;
-        light.shadow.mapSize.height = 2048;
-        scene.add(light);
-        for (let i = 0; i < attention; i++) {
-            let object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
-
-            object.position.x = Math.random() * 1000 - 500;
-            object.position.y = Math.random() * 600 - 300;
-            object.position.z = Math.random() * 800 - 400;
-
-            object.rotation.x = Math.random() * 2 * Math.PI;
-            object.rotation.y = Math.random() * 2 * Math.PI;
-            object.rotation.z = Math.random() * 2 * Math.PI;
-
-            object.scale.x = Math.random() * 2 + 1;
-            object.scale.y = Math.random() * 2 + 1;
-            object.scale.z = Math.random() * 2 + 1;
-
-            object.castShadow = true;
-            object.receiveShadow = true;
-            object.name = 'cube' + i;
-
-            scene.add(object);
-            objects.push(object);
-    }
-}
-
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-}
-
-function animate() {
-
-    requestAnimationFrame(animate);
-
-    render();
-
-}
-
-function render() {
-    // controls.update();
-
-    renderer.render(scene, camera);
-
+document.body.onload = function(){
+  muppeteer = new Muppeteer();
+  // console.log('muppeteer: ');
+  // console.dir(muppeteer);
 }
